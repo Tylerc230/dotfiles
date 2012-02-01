7cf3d7928797cf7271beb59423c7809170c546aa
Tyler Casselman <tylerc230@gmail.com>
2012-02-01 09:30:45 -0800

    Playing with themes, fixing zsh setup script

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,4 +1,4 @@
-sudo brew install zsh
+brew install zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-ln -s ~/dotfiles/zsh/zshrc ~/.zsh
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
