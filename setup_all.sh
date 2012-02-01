a9a8375ece265080e886943c804d3e19d961be3c
Tyler Casselman <tcasselman@zynga.com>
2012-01-31 17:22:34 -0800

    adding zsh to setup

--- setup_all.sh
+++ setup_all.sh
@@ -4,7 +4,9 @@ rm -f ~/.vimrc
 touch ~/.profile
 sh ~/dotfiles/bash/setup_bash.sh
 sh ~/dotfiles/vim/setup_vim.sh
+sh ~/dotfiles/zsh/setup_zsh.sh
 pushd ~/dotfiles/vim/bundles/command-t/ruby/command-t/
 rvm use system
 ruby extconf.rb
 make
+popd
---
+++ zsh/setup_zsh.sh
@@ -0,0 +1,2 @@
+sudo brew install zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
