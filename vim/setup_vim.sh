02868ac17f094dc9c507825e2498f6113bf56d6f
Tyler Casselman <tyler@Tylers-MacBook-Air.local>
2016-06-15 17:19:40 -0700

    install command line tools in setup

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,4 +1,5 @@
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+ln -s ~/dogenerictfiles/vim/vimrc ~/.vimrc
 pushd ~/dotfiles/
 git submodule init
 git submodule update
