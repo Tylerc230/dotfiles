813c7ad4d76a6b06a14e9dbc645c76ba5bf009cd
Tyler Casselman <tylerc230@gmail.com>
2019-03-18 09:00:44 -0700

    Add chunkwm to hammerspoon

--- hammerspoon/hammerspoon/init.lua
+++ hammerspoon/hammerspoon/init.lua
@@ -1,4 +1,5 @@
 local hyper = {"cmd", "alt", "ctrl"}
+local superHyper = {"shift", "cmd", "alt", "ctrl"}
 
 -- application launching 
 hs.hotkey.bind(hyper, 't', function()
@@ -7,9 +8,6 @@ end)
 
 hs.hotkey.bind(hyper, 'v', function()
   hs.application.launchOrFocus('Xcode')
-end)
-hs.hotkey.bind(hyper, 'h', function()
-  hs.application.launchOrFocus('Finder')
 end)
 hs.hotkey.bind(hyper, 's', function()
   hs.application.launchOrFocus('Slack')
@@ -26,56 +24,79 @@ end)
 hs.hotkey.bind(hyper, 'n', function()
   hs.application.launchOrFocus('Notes')
 end)
---hs.hotkey.bind("cmd", 'space', function()
-  --hs.application.launchOrFocus('Kitty')
---end)
--- window movement
+
+hs.hotkey.bind(hyper, 'b', function()
+  wm("tiling::desktop --layout bsp")
+end)
+hs.hotkey.bind(hyper, 'f', function()
+  wm("tiling::desktop --layout monocle")
+end)
+
+hs.hotkey.bind(hyper, 'g', function()
+  wm("tiling::desktop --layout float")
+end)
+
+--focus
+hs.hotkey.bind(hyper, 'h', function()
+  wm("tiling::window --focus west")
+end)
+hs.hotkey.bind(hyper, 'j', function()
+  wm("tiling::window --focus south")
+end)
+hs.hotkey.bind(hyper, 'k', function()
+  wm("tiling::window --focus north")
+end)
+hs.hotkey.bind(hyper, 'l', function()
+  wm("tiling::window --focus east")
+end)
+
 hs.hotkey.bind(hyper, "LEFT", function()
-  local win = hs.window.focusedWindow()
-  moveWindowLeft(win)
-  fullscreen(win)
-end, nil, function()
-  local win = hs.window.focusedWindow()
-  fillLeft(win)
+  wm("tiling::monitor -f 2")
 end)
 
 hs.hotkey.bind(hyper, "RIGHT", function()
-  local win = hs.window.focusedWindow()
-  moveWindowRight(win)
-  fullscreen(win)
+  wm("tiling::monitor -f 1")
 end)
 
-hs.hotkey.bind(hyper, "F", function()
-  local win = hs.window.focusedWindow()
-  fullscreen(win)
+--moving windows
+hs.hotkey.bind(hyper, 'x', function()
+  wm('tiling::window --close')
+end)
+hs.hotkey.bind(superHyper, 'h', function()
+  wm("tiling::window --swap west")
+end)
+hs.hotkey.bind(superHyper, 'j', function()
+  wm("tiling::window --swap south")
+end)
+hs.hotkey.bind(superHyper, 'k', function()
+  wm("tiling::window --swap north")
+end)
+hs.hotkey.bind(superHyper, 'l', function()
+  wm("tiling::window --swap east")
 end)
 
-function moveWindowLeft(window)
-  window:moveOneScreenWest()
-end
+hs.hotkey.bind(superHyper, "LEFT", function()
+  wm("tiling::window --send-to-monitor 2")
+end)
 
-function moveWindowRight(window)
-  window:moveOneScreenEast()
-end
+hs.hotkey.bind(superHyper, "RIGHT", function()
+  wm("tiling::window --send-to-monitor 1")
+end)
 
-function fullscreen(window)
-  fillPercent(window, 1.0, 1.0)
-end
 
-function fillLeft(window)
-  fillPercent(window, .5, 1.0)
-end
 
-function fillPercent(window, w, h)
-  local f = window:frame()
-  local screen = window:screen()
-  local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w * w
-  f.h = max.h * h
-  window:setFrame(f)
+--hs.hotkey.bind(hyper, "F", function()
+	--wm("tiling::window --toggle fullscreen")
+--end)
+
+
+function wm(command)
+  local output, status, type, rc = hs.execute("/usr/local/bin/chunkc "..command, false)
+  print(output)
+	print(status)
+	print(type)
+	print(rc)
 end
 
 function reloadConfig(files)
