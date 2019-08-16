f232a946e7dcc9e51dcce2a4a4e7155756efb1a2
Tyler Casselman <tylerc230@gmail.com>
2019-08-16 11:27:11 -0700

    Update help text

--- scripts/authorlog.sh
+++ scripts/authorlog.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ -z "$1" ]
 then
-    echo "git-author-log v1.0"
     echo "
 Shows the git log by order of AuthorDate instead of CommitDate."
     echo ""
     echo "Usage:"
-    echo "git author-log"
+    echo "author-log.sh <since>"
     exit 0
 fi
 git log --all --author=tyler --since=$1 --pretty="format:%at %C(yellow)commit %H%Creset\nAuthor: %an <%ae>\nDate: %aD\n\n %s\n" | sort -r | cut -d" " -f2- | sed -e "s/\\\n/\\`echo -e '\n\r'`/g" | tr -d '\15\32' | less -R
