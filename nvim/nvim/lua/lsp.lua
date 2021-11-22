883dae3201e3d8af4eefd9a8a648c1e111696c5a
Tyler Casselman <cstyle@amazon.com>
2021-11-22 08:50:05 -0800

    Fix swift/cpp/c/objc lsp config

--- nvim/nvim/lua/lsp.lua
+++ nvim/nvim/lua/lsp.lua
@@ -7,45 +7,121 @@ local function setup_servers()
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
     require'lspconfig'[server].setup{
-      --capabilities = capabilities,
+      capabilities = capabilities,
     }
   end
-end
 
-setup_servers()
-local lsp_config = require'lspconfig'
-local util = require'lspconfig'.util
-lsp_config.sourcekit.setup {
-  cmd = { "xcrun", "sourcekit-lsp" },
-  root_dir = util.root_pattern("Package.swift", ".git"),
-  --filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp", "objc" }
-  filetypes = { "swift" }
-}
+  local lsp_config = require'lspconfig'
+  local util = require'lspconfig'.util
+  lsp_config.sourcekit.setup {
+    cmd = { "xcrun", "sourcekit-lsp" },
+    root_dir = util.root_pattern("Package.swift", ".git"),
+    --filetypes = { "swift", "c", "cpp", "objective-c", "objective-cpp", "objc" }
+    filetypes = { "swift" }
+  }
 
-lsp_config.clangd.setup {
-  --cmd = { "/usr/local/Cellar/llvm/12.0.1/bin/clangd", "-log=verbose" },
-  --cmd = { "xcrun", "clangd", "-log=verbose" },
-  cmd = { "xcrun", "clangd" },
-  root_dir = util.root_pattern("compile_commands.json", ".git"),
-  filetypes = { "c", "cpp", "objective-c", "objective-cpp", "objc" }
-}
+  lsp_config.clangd.setup {
+    --cmd = { "/usr/local/Cellar/llvm/12.0.1/bin/clangd", "-log=verbose" },
+    --cmd = { "xcrun", "clangd", "-log=verbose" },
+    cmd = { "xcrun", "clangd" },
+    root_dir = util.root_pattern("compile_commands.json"),
+    filetypes = { "c", "cpp", "objective-c", "objective-cpp", "objc" }
+  }
+
 lsp_config.rust_analyzer.setup({
     on_attach=on_attach,
     settings = {
-        ["rust-analyzer"] = {
-            assist = {
-                importGranularity = "module",
-                importPrefix = "by_self",
-            },
-            cargo = {
-                loadOutDirsFromCheck = true
-            },
-            procMacro = {
-                enable = true
-            },
-        }
+      ["rust-analyzer"] = {
+        linkedProjects = {
+          --order is important (rust-analyzer/rust-analyzer#7764)
+          "Cargo.toml",
+          "avr/Cargo.toml",
+        },
+        assist = {
+          importGranularity = "module",
+          importPrefix = "by_self",
+        },
+        cargo = {
+          loadOutDirsFromCheck = true,
+          target = "avr-unknown-gnu-atmega328"
+        },
+
+        diagnostics = {
+          disabled = {
+            --https://github.com/rust-analyzer/rust-analyzer/issues/6835
+            "unresolved-macro-call"
+          }
+        },
+        procMacro = {
+          enable = true
+        },
+      }
     }
-})
+  })
+
+
+  --local opts = {
+    --tools = { -- rust-tools options
+      --autoSetHints = true,
+      --hover_with_actions = true,
+      --inlay_hints = {
+        --show_parameter_hints = false,
+        --parameter_hints_prefix = "",
+        --other_hints_prefix = "",
+      --},
+    --},
+
+    ---- all the opts to send to nvim-lspconfig
+    ---- these override the defaults set by rust-tools.nvim
+    ---- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+    --server = {
+      ---- on_attach is a callback called when the language server attachs to the buffer
+      --on_attach = on_attach,
+      --settings = {
+        ---- to enable rust-analyzer settings visit:
+        ---- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+        --["rust-analyzer"] = {
+            --assist = {
+                --importGranularity = "module",
+                --importPrefix = "by_self",
+            --},
+            --cargo = {
+                --loadOutDirsFromCheck = true
+            --},
+            --procMacro = {
+                --enable = true
+            --},
+          ---- enable clippy on save
+          --checkOnSave = {
+            --command = "clippy"
+          --},
+        --}
+      --}
+    --},
+  --}
+
+  --require('rust-tools').setup(opts)
+
+  --lsp_config.rust.setup({
+  --on_attach=on_attach,
+  --settings = {
+  --["rust-analyzer"] = {
+  --assist = {
+  --importGranularity = "module",
+  --importPrefix = "by_self",
+  --},
+  --cargo = {
+  --loadOutDirsFromCheck = true
+  --},
+  --procMacro = {
+  --enable = true
+  --},
+  --}
+--}
+  --})
+end
+
+setup_servers()
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
 require'lspinstall'.post_install_hook = function ()
   setup_servers() -- reload installed servers
