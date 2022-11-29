97f029b4924cd40bffd2ed2d3e220392ce457273
Tyler Casselman <cstyle@amazon.com>
2022-11-29 14:31:20 -0800

    Remove deprecated methods in nvim

--- nvim/nvim/lua/lsp.lua
+++ nvim/nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.setup()
 local lsp_config = require("lspconfig")
