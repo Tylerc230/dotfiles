296ab9ea2c3be752a2af99f087ce88a82075a2cb
Tyler Casselman <cstyle@amazon.com>
2024-12-04 10:40:50 -0800

    Close telescope with q instead of esc

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -358,6 +358,7 @@ local git_browser_actions = transform_mod({
 })
 
 require('telescope').load_extension("live_grep_args")
+local actions = require("telescope.actions")
 require('telescope').setup {
   defaults = {
     path_display = {'smart'},
@@ -366,11 +367,15 @@ require('telescope').setup {
         ["<C-h>"] = "which_key",
         ["<Down>"] = require('telescope.actions').cycle_history_next,
         ["<Up>"] = require('telescope.actions').cycle_history_prev,
+        ["<C-c>"] = actions.close,
       },
       n = {
         ["<C-h>"] = "which_key",
         ["<Down>"] = require('telescope.actions').cycle_history_next,
         ["<Up>"] = require('telescope.actions').cycle_history_prev,
+        ["<C-c>"] = actions.close,
+        ["q"] = actions.close,
+        ["<esc>"] = false,
       },
     },
   },
@@ -449,9 +454,47 @@ require'nvim-treesitter.configs'.setup {
       -- * query_string: eg '@function.inner'
       -- * selection_mode: eg 'v'
       -- and should return true or false
-      include_surrounding_whitespace = true,
+      include_surrounding_whitespace = false,
     },
-  }
+  },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      ["]m"] = "@function.outer",
+      ["]]"] = { query = "@class.outer", desc = "Next class start" },
+      --
+      -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+      ["]o"] = "@loop.*",
+      -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+      --
+      -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+      -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+      ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
+      ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+    },
+    goto_next_end = {
+      ["]M"] = "@function.outer",
+      ["]["] = "@class.outer",
+    },
+    goto_previous_start = {
+      ["[m"] = "@function.outer",
+      ["[["] = "@class.outer",
+    },
+    goto_previous_end = {
+      ["[M"] = "@function.outer",
+      ["[]"] = "@class.outer",
+    },
+    -- Below will go to either the start or the end, whichever is closer.
+    -- Use if you want more granular movements
+    -- Make it even more gradual by adding multiple queries and regex.
+    goto_next = {
+      ["]d"] = "@conditional.outer",
+    },
+    goto_previous = {
+      ["[d"] = "@conditional.outer",
+    }
+  },
 }
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 --parser_config.objc = {
