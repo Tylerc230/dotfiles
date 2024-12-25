1b70b438aaa0648fb7b996c0c2a1d711e6742778
Tyler Casselman <tylerc230@gmail.com>
2024-12-25 10:42:56 -0700

    Move windows, remove autoformat, fugitive keybindings

--- nvim/LazyVim/lua/config/keymaps.lua
+++ nvim/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+--- use `vim.keymap.set` instead
+local map = vim.keymap.set
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<leader>h", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<leader>j", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<leader>k", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<leader>l", "<C-w>l", { desc = "Go to Right Window", remap = true })
--- nvim/LazyVim/lua/plugins/vim-fugitive.lua
+++ nvim/LazyVim/lua/plugins/vim-fugitive.lua
@@ -1,7 +1,8 @@
 return {
   {
     "tpope/vim-fugitive",
-    keys = { { "<leader>gs", ":Git<CR>", desc = "Git Status" } },
-    -- lazy = true
+    -- keys = { { "<leader>gs", ":Git<CR>", desc = "Git Status" } }, -- need to disable fzf git status
+    keys = { { "<leader>gS", ":Git<CR>", desc = "Fugitive Status" } },
+    -- lazy = true,
   },
 }
