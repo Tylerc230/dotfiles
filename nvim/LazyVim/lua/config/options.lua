8b84cf8118c5198a02e0928092655fd1b0ca3021
Tyler Casselman <tylerc230@gmail.com>
2024-12-27 10:17:58 -0700

    Set windowpicker colors

--- nvim/LazyVim/lua/config/options.lua
+++ nvim/LazyVim/lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+vim.api.nvim_set_hl(0, 'WindowPickerStatusLine', { link = 'Search' })
+vim.api.nvim_set_hl(0, 'WindowPickerWinBar', { link = 'Search' })
+vim.api.nvim_set_hl(0, 'WindowPickerStatusLineNC', { link = 'DiffDelete' })
+vim.api.nvim_set_hl(0, 'WindowPickerWinBarNC', { link = 'DiffDelete' })
+
+
+
--- nvim/LazyVim/lua/plugins/nvim-window-picker.lua
+++ nvim/LazyVim/lua/plugins/nvim-window-picker.lua
@@ -1,43 +1,45 @@
+local function choose_win()
+  return function()
+    local window_id = require("window-picker").pick_window()
+    if window_id == nil then
+      return
+    end
+    vim.api.nvim_set_current_win(window_id)
+  end
+end
+
+local function close_win()
+  return function()
+    local window_id = require("window-picker").pick_window({ filter_rules = { autoselect_one = false } })
+    if window_id == nil then
+      return
+    end
+    vim.api.nvim_win_close(window_id, false)
+  end
+end
 return {
   {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
     opt = {
       show_prompt = false,
     },
     config = function()
-      require'window-picker'.setup({
+      require("window-picker").setup({
         show_prompt = false,
         filter_rules = {
           bo = {
             -- if the file type is one of following, the window will be ignored
-            filetype = { 'notify' },
-
-          },
-        },
-        highlights = {
-          statusline = {
-            unfocused = {
-              fg = '#ededed',
-              bg = '#444444',
-              bold = true,
-            },
-          },
-          winbar = {
-            unfocused = {
-              fg = '#ededed',
-              bg = '#444444',
-              bold = true,
-            },
+            filetype = { "notify" },
           },
         },
       })
     end,
     keys = {
-      {"<leader>ww", "<cmd>lua vim.api.nvim_set_current_win(require('window-picker').pick_window())<CR>"}
-
-    }
-  }
+      { "<leader>ww", choose_win() },
+      { "<leader>wd", close_win() },
+    },
+  },
 }
