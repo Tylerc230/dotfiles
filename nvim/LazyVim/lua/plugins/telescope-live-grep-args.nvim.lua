d6f96fb23d496927529d7159de87be2e6c9e452c
Tyler Casselman <cstyle@amazon.com>
2025-01-11 07:08:09 -0800

    Add live grep args telescope

---
+++ nvim/LazyVim/lua/plugins/telescope-live-grep-args.nvim.lua
@@ -0,0 +1,44 @@
+return {
+  { "nvim-telescope/telescope-live-grep-args.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        config = function()
+          LazyVim.on_load("telescope.nvim", function()
+            require(
+              "telescope"
+            ).load_extension("live_grep_args")
+          end)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require(
+            "telescope"
+          ).extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Grep with Args (root dir)",
+      },
+    },
+    opts = {
+      extensions = {
+        live_grep_args = {
+          mappings = {
+            i = {
+              ["<C-k>"] = function(picker)
+                require(
+                  "telescope-live-grep-args.actions"
+                ).quote_prompt()(picker)
+              end,
+            },
+          },
+        },
+      },
+    },
+  }
+}
