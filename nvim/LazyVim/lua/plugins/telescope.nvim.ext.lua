41c332c11b9b179654f97faf7d45ca7193023a87
Tyler Casselman <cstyle@amazon.com>
2025-01-25 15:45:23 -0800

    Updates to telescope

--- nvim/LazyVim/lua/plugins/telescope.nvim.ext.lua
+++ nvim/LazyVim/lua/plugins/telescope.nvim.ext.lua
@@ -1,19 +1,24 @@
 local actions = require("telescope.actions")
 return {
   "nvim-telescope/telescope.nvim",
+  keys = {
+
+      { "<leader>gC", "<cmd>Telescope git_bcommits<CR>", desc = "Buffer Commits" },
+  },
   opts = {
     defaults = {
+      path_display = { "smart" },
       mappings = {
         i = {
           ["<C-h>"] = "which_key",
-          ["<Down>"] = actions.cycle_history_next,
-          ["<Up>"] = actions.cycle_history_prev,
+          ["<C-j>"] = actions.cycle_history_next,
+          ["<C-k>"] = actions.cycle_history_prev,
           ["<C-c>"] = actions.close,
         },
         n = {
           ["<C-h>"] = "which_key",
-          ["<Down>"] = actions.cycle_history_next,
-          ["<Up>"] = actions.cycle_history_prev,
+          ["<C-j>"] = actions.cycle_history_next,
+          ["<C-k>"] = actions.cycle_history_prev,
           ["<C-c>"] = actions.close,
           ["q"] = actions.close,
           ["<esc>"] = false,
