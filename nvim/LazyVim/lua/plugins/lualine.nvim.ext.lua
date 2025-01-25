211d23914e54d2606be6d751cfd7eaf4d0750c9b
Tyler Casselman <cstyle@amazon.com>
2025-01-25 15:45:40 -0800

    Updates to lualine, add winsep

---
+++ nvim/LazyVim/lua/plugins/lualine.nvim.ext.lua
@@ -0,0 +1,23 @@
+local icons = LazyVim.config.icons
+-- Could also return function(opts) and replace the last element in opts.sections.lualine_c
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    sections = {
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path({length = 0}) },
+      },
+    },
+  },
+}
