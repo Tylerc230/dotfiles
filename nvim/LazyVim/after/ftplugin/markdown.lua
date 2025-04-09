1b825eaa1f58f668113bcb8f127a380be374e6e0
Tyler Casselman <cstyle@amazon.com>
2025-04-09 12:11:15 -0700

    Add ftplugins folder (some not working)

---
+++ nvim/LazyVim/after/ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+vim.opt.wrap = false -- this doesn't
+vim.opt.shiftwidth = 2 -- this works
+print("MARKDOWN LOADED")
---
+++ nvim/LazyVim/after/ftplugin/text.vim
@@ -0,0 +1,3 @@
+set wrap=0
+noremap <buffer> <silent> k gk
+noremap <buffer> <silent> j gj
